bff4e0798c427e0b8f6e19dd4e6b04e2dd420cb9
Neil Girari <neil.girardi@gmail.com>
2017-09-03 23:12:15 -0400

    Added new questions to mock of database.

--- src/backend/questions-and-answers.js
+++ src/backend/questions-and-answers.js
@@ -69,6 +69,177 @@ module.exports = exports = [
       }
     ],
     "correctAnswer": "A"
+  },
+  {
+    "ID": 3,
+    "question": "Who invented the mouse?",
+    "multipleChoices": [
+      {
+        "ID": "A",
+        "answer": "Steve Wozniak"
+      },
+      {
+        "ID": "B",
+        "answer": "Douglas Englebart"
+      },
+      {
+        "ID": "C",
+        "answer":  "Ed Roberts"
+      },
+      {
+        "ID": "D",
+        "answer": "Bob Biniak"
+      }
+    ],
+    "correctAnswer": "B"
+  },
+  {
+    "ID": 4,
+    "question": "in 1969, the first electronic message was sent " +
+    "between two remote computers. The message said ...?",
+    "multipleChoices": [
+      {
+        "ID": "A",
+        "answer": "'hello world'"
+      },
+      {
+        "ID": "B",
+        "answer": "'test'"
+      },
+      {
+        "ID": "C",
+        "answer":  "'lo'"
+      },
+      {
+        "ID": "D",
+        "answer": "'login'"
+      }
+    ],
+    "correctAnswer": "C"
+  },
+  {
+    "ID": 5,
+    "question": "The programming language Python owes its name to the " +
+    "creator's appreciation of  ...?",
+    "multipleChoices": [
+      {
+        "ID": "A",
+        "answer": "The ancient Greek mathematician Pythagoras"
+      },
+      {
+        "ID": "B",
+        "answer": "snakes"
+      },
+      {
+        "ID": "C",
+        "answer":  "The British television show Monty Python"
+      },
+      {
+        "ID": "D",
+        "answer": "The mathematical constant Pi"
+      }
+    ],
+    "correctAnswer": "C"
+  },
+  {
+    "ID": 6,
+    "question": "Which of the following statements about the web scripting " +
+    "language PHP is NOT true?",
+    "multipleChoices": [
+      {
+        "ID": "A",
+        "answer": "Its creator hails from Finland"
+      },
+      {
+        "ID": "B",
+        "answer": "It was originally intended to be used as a templating language " +
+        "for websites written in C / C++"
+      },
+      {
+        "ID": "C",
+        "answer":  "Its creator works at Etsy"
+      },
+      {
+        "ID": "D",
+        "answer": "The acronym PHP originally stood for Personal Home Page"
+      }
+    ],
+    "correctAnswer": "A"
+  },
+  {
+    "ID": 7,
+    "question": "Which programming language first popularized the MVC design " +
+    "pattern?",
+    "multipleChoices": [
+      {
+        "ID": "A",
+        "answer": "Ruby"
+      },
+      {
+        "ID": "B",
+        "answer": "C#"
+      },
+      {
+        "ID": "C",
+        "answer":  "Java"
+      },
+      {
+        "ID": "D",
+        "answer": "Smalltalk"
+      }
+    ],
+    "correctAnswer": "D"
+  },
+  {
+    "ID": 8,
+    "question": "Which well known software developer is also an accomplished " +
+    "race car driver?",
+    "multipleChoices": [
+      {
+        "ID": "A",
+        "answer": "Guido Van Rossum"
+      },
+      {
+        "ID": "B",
+        "answer": "David Heinemeier Hansson"
+      },
+      {
+        "ID": "C",
+        "answer":  "John McAfee"
+      },
+      {
+        "ID": "D",
+        "answer": "Eric S. Raymond"
+      }
+    ],
+    "correctAnswer": "B"
+  },
+  {
+    "ID": 9,
+    "question": "The Balmer Peak is ...?",
+    "multipleChoices": [
+      {
+        "ID": "A",
+        "answer": "The idea that a programmer's productivity may increase " +
+        "significantly after a few alcoholic beverages"
+      },
+      {
+        "ID": "B",
+        "answer": "The business Management philosophy of Microsoft executive " +
+        "Steve Balmer"
+      },
+      {
+        "ID": "C",
+        "answer":  "A growth curve of O(log N)"
+      },
+      {
+        "ID": "D",
+        "answer": "A term coined by tech journalists to describe Steve Balmer's " +
+        "stewardship of  Microsoft as CEO"
+      }
+    ],
+    "correctAnswer": "A"
   }
-];
 
+
+];
